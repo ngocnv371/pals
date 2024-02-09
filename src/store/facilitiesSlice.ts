858b6fa6cc2b84e9ed665476fab0a16b316abecd
Ngoc Ngo <ngocnv371@gmail.com>
2024-02-09 20:15:00 +0700

    add default facility

--- src/store/facilitiesSlice.ts
+++ src/store/facilitiesSlice.ts
@@ -10,13 +10,24 @@ import { itemAdded } from "./inventorySlice";
 
 const facilitiesAdapter = createEntityAdapter<Facility>();
 
-const initialState = facilitiesAdapter.getInitialState();
+const fa0: Facility = {
+  id: nanoid(),
+  type: "Logging Site",
+  baseId: "default",
+};
+
+const initialState = facilitiesAdapter.getInitialState({
+  entities: {
+    [fa0.id]: fa0,
+  },
+  ids: [fa0.id],
+});
 
 export const facilitiesSlice = createSlice({
   name: "facilities",
   initialState,
   reducers: {
-     added: facilitiesAdapter.addOne
+    added: facilitiesAdapter.addOne,
   },
 });
 
@@ -28,18 +39,20 @@ export const {
 export const selectFacilitiesByBaseId = createSelector(
   [selectAllFacilities, (_, baseId: string) => baseId],
   (facilities, baseId) => {
-    return facilities.filter((f) => f.baseId === baseId)
+    return facilities.filter((f) => f.baseId === baseId);
   }
 );
 
-export const facilityCreated = (type: string, baseId: string) => (dispatch: AppDispatch) => {
-  dispatch(
-    facilitiesSlice.actions.added({
-      id: nanoid(),
-      type, baseId
-    })
-  );
-};
+export const facilityCreated =
+  (type: string, baseId: string) => (dispatch: AppDispatch) => {
+    dispatch(
+      facilitiesSlice.actions.added({
+        id: nanoid(),
+        type,
+        baseId,
+      })
+    );
+  };
 
 export const worked = (facilityId: string) => (dispatch: AppDispatch) => {
   dispatch(itemAdded({ id: "stone", quantity: 1 }));
