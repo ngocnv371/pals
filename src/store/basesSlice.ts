9b79ac2779ce4a51d186e1467ed896c03d02c8d0
Ngoc Ngo <ngocnv371@gmail.com>
2024-02-09 10:23:11 +0700

    can create base

--- src/store/basesSlice.ts
+++ src/store/basesSlice.ts
@@ -1,25 +1,37 @@
-import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+import { createSlice, createEntityAdapter, nanoid } from "@reduxjs/toolkit";
 import { Base } from "../models/base";
+import { AppDispatch } from "./store";
 
 const basesAdapter = createEntityAdapter<Base>();
 
 const initialState = basesAdapter.getInitialState({
   entities: {
     default: {
-      id: 'default',
-      name: 'Default Base'
-    }
+      id: "default",
+      name: "Default Base",
+    },
   },
-  ids: ['default']
+  ids: ["default"],
 });
 
 export const basesSlice = createSlice({
   name: "bases",
   initialState,
-  reducers: {},
+  reducers: {
+    added: basesAdapter.addOne,
+  },
 });
 
 export const { selectAll: selectAllBases, selectById: selectBaseById } =
   basesAdapter.getSelectors();
+
+export const baseCreated = (name: string) => (dispatch: AppDispatch) => {
+  dispatch(
+    basesSlice.actions.added({
+      id: nanoid(),
+      name,
+    })
+  );
+};
 
 export default basesSlice.reducer;
