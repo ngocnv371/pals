595c75ccc8b0a5007af49eefc953fa4ea300154b
Ngoc Ngo <ngocnv371@gmail.com>
2024-03-01 10:20:02 +0700

    correct breed alpha

--- src/data/palBreed.ts
+++ src/data/palBreed.ts
@@ -1,9 +1,11 @@
 import combinations from "./combinations.json";
 
 export default function breed(parent1Name: string, parent2Name: string) {
+  const p1 = parent1Name.replace("Alpha", "").trim();
+  const p2 = parent2Name.replace("Alpha", "").trim();
   return combinations.find(
     (c) =>
-      (c.parent1Name == parent1Name && c.parent2Name == parent2Name) ||
-      (c.parent2Name == parent1Name && c.parent1Name == parent2Name)
+      (c.parent1Name == p1 && c.parent2Name == p2) ||
+      (c.parent2Name == p1 && c.parent1Name == p2)
   )?.childName;
 }
