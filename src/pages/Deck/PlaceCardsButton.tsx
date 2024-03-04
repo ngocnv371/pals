39d94be17f5f6730a7c56c2709532ad79638d82f
Ngoc Ngo <ngocnv371@gmail.com>
2024-03-04 10:16:19 +0700

    must select cards

--- src/pages/Deck/PlaceCardsButton.tsx
+++ src/pages/Deck/PlaceCardsButton.tsx
@@ -1,10 +1,17 @@
 import { IonFab, IonFabButton, IonIcon } from "@ionic/react";
 import { chevronForward } from "ionicons/icons";
-import { useAppDispatch } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { myPlacingStarted } from "../../store/duelSlice";
 
 const PlaceCardsButton: React.FC = () => {
   const dispatch = useAppDispatch();
+  const selected = useAppSelector(
+    (state) => state.duel.my.deploymentPlan?.selectedReservesIndices
+  );
+
+  if (!selected || !selected.length) {
+    return null;
+  }
 
   return (
     <IonFab horizontal="end" vertical="bottom">
