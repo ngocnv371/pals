53608c0b3ef30f967f2135f1963f47bffe91cc7a
Ngoc Ngo <ngocnv371@gmail.com>
2024-03-07 11:11:37 +0700

    add fusion spark effect

--- src/pages/Duel/Effects/SparkEffect.tsx
+++ src/pages/Duel/Effects/SparkEffect.tsx
@@ -2,14 +2,15 @@ import Sprite from "./Sprite";
 
 const SparkEffect: React.FC = () => {
   return (
-    <Sprite
-      url="url(/effects/spark-sprite.png)"
-      frames={8}
-      tileWidth={256}
-      cols={4}
-      duration={120}
-      repeat
-    />
+    <div className="animate__animated animate__fadeOut animate__slower">
+      <Sprite
+        url="url(/effects/spark-sprite.png)"
+        frames={8}
+        tileWidth={256}
+        cols={4}
+        duration={120}
+      />
+    </div>
   );
 };
 
--- src/pages/Duel/Fusion/FusionVisualizer.tsx
+++ src/pages/Duel/Fusion/FusionVisualizer.tsx
@@ -1,8 +1,9 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAppSelector } from "../../../store/hooks";
 import { useClassSequence } from "../utils/useClassSequence";
 import "./FusionVisualizer.css";
 import { CardInfo } from "../../../components/Card/CardInfo";
+import SparkEffect from "../Effects/SparkEffect";
 
 const segments = [
   { className: "fusion-visualizer--intro", duration: 1000 },
@@ -19,12 +20,27 @@ const _FusionVisualizer: React.FC<{
 }> = ({ card1, card2, card3, onCompleted }) => {
   const ref = useRef<HTMLDivElement>();
   useClassSequence(ref, segments, onCompleted);
+  const [showEffect, setShowEffect] = useState(false);
+
+  // schedule effect
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowEffect(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className="fusion-visualizer" ref={ref as any}>
       <CardInfo cardId={card1} />
       <CardInfo cardId={card2} />
       <CardInfo cardId={card3} />
+      {showEffect && (
+        <div className="effect">
+          <SparkEffect />
+        </div>
+      )}
     </div>
   );
 };
