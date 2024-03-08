fc1def56fce93a113eac40d4cd3bdefc08398fbe
Ngoc Ngo <ngocnv371@gmail.com>
2024-03-08 16:06:09 +0700

    display card name

--- src/components/Card/CardInfo.tsx
+++ src/components/Card/CardInfo.tsx
@@ -10,10 +10,13 @@ export const CardInfo: React.FC<{ cardId: string }> = ({ cardId }) => {
     return null;
   }
 
+  const longTitle = card.title.length > 10;
+
   return (
     <IonCard className="card-info">
       <IonImg src={`/pals/${card.content.image}`}></IonImg>
-      <p>
+      <p className={`title ${longTitle ? "long" : ""}`}>{card.title}</p>
+      <p className="power">
         <span className="atk">{card.content.baseAttack}</span>/
         <span className="def">{card.content.defense}</span>
       </p>
