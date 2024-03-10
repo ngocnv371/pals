a14f44d94163d7bfe4ae9d53342440fa7e03e43e
Ngoc Ngo <ngocnv371@gmail.com>
2024-03-10 16:19:18 +0700

    improved copy text

--- src/pages/MonsterFactory/ChatMessage.tsx
+++ src/pages/MonsterFactory/ChatMessage.tsx
@@ -3,18 +3,33 @@ import {
   IonCardHeader,
   IonCardTitle,
   IonCardContent,
+  IonButton,
+  IonIcon,
 } from "@ionic/react";
 import { ChatCompletionMessage } from "../../gpt/model";
+import { copyOutline } from "ionicons/icons";
+import { useCallback, useState } from "react";
 
 const ChatMessageCard: React.FC<{ message: ChatCompletionMessage }> = ({
   message,
 }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = useCallback(() => {
+    navigator.clipboard.writeText(message.content);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  }, [message]);
+
   return (
     <IonCard>
       <IonCardHeader>
         <IonCardTitle>{message.role}</IonCardTitle>
       </IonCardHeader>
       <IonCardContent className="prompt">{message.content}</IonCardContent>
+      <IonButton fill="clear" onClick={handleCopy} disabled={copied}>
+        <IonIcon slot="start" icon={copyOutline} /> Copy
+      </IonButton>
     </IonCard>
   );
 };
--- src/pages/MonsterFactory/MonsterPrompt.tsx
+++ src/pages/MonsterFactory/MonsterPrompt.tsx
@@ -1,15 +1,5 @@
-import {
-  IonButton,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardTitle,
-  IonIcon,
-  IonList,
-} from "@ionic/react";
-import { copy, download } from "ionicons/icons";
 import { Monster } from "./model";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { generateMessages } from "./service";
 import { ChatCompletionMessage } from "../../gpt/model";
 import ChatMessageCard from "./ChatMessage";
@@ -17,7 +7,6 @@ import SmartFillButton from "./SmartFillButton";
 import UploadButton from "./UploadButton";
 
 const MonsterPrompt: React.FC<{ monster: Monster }> = ({ monster }) => {
-  const [copied, setCopied] = useState(false);
   const [messages, setMessages] = useState<ChatCompletionMessage[]>([]);
   const [generatedMessage, setGeneratedMessage] =
     useState<ChatCompletionMessage | null>(null);
@@ -30,17 +19,8 @@ const MonsterPrompt: React.FC<{ monster: Monster }> = ({ monster }) => {
     setMessages(msgs.reverse());
   }, [monster, generatedMessage]);
 
-  const handleCopy = useCallback(() => {
-    navigator.clipboard.writeText(messages[2].content);
-    setCopied(true);
-    setTimeout(() => setCopied(false), 2000);
-  }, [messages]);
-
   return (
     <div className="monster-prompt">
-      <IonButton fill="clear" onClick={handleCopy} disabled={copied}>
-        <IonIcon slot="start" icon={copy} /> Copy Prompt
-      </IonButton>
       <SmartFillButton
         monster={monster}
         onCompleted={(msg) => setGeneratedMessage(msg)}
